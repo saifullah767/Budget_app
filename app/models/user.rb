19b0c79fc8e46c53080d0376479e66e090734a72
saifullah767 <skj99667788@gmail.com>
2022-09-07 03:04:00 +0500

    commit deature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+
     has_many: groups, dependent: :destroy
     has_many: transactions, dependent: :destroy
+
+    
+    validates:name, presense:true
+
+
 end
--- db/migrate/20220906201158_create_transactions.rb
+++ db/migrate/20220906201158_create_transactions.rb
@@ -3,7 +3,7 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
     create_table :transactions do |t|
 
       t.text :name
-      t.float :float default 0.0
+      t.float :amount, default: 0.0
 
       t.timestamps
     end
