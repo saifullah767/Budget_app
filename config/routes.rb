e355beef255abbdb4bf53e51c2446f57a97ef9ca
saifullah767 <skj99667788@gmail.com>
2022-09-07 03:46:36 +0500

    Update: Group controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@
 
 Rails.application.routes.draw do
 
-  get 'groups/index'
-  get 'group/index'
-  get 'transactions/index'
-  get 'users/index'
-  
+  # get 'groups/index'
+  # get 'group/index'
+  # get 'transactions/index'
+  # get 'users/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'users#index'
+  resources :users
+  resources :groups do
+    resources :transactions
+  end
 end
