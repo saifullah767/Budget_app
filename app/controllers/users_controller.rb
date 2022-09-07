24e9dc25581ab31b3be9abcb23976c2cb42a67bd
saifullah767 <skj99667788@gmail.com>
2022-09-07 20:31:17 +0500

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = params[:user_id]
+    @users = params[user_id: current_user.id]
   end
 end
