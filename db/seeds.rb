14c3567a8745965e712a3429770e10b213c79c1b
saifullah767 <skj99667788@gmail.com>
2022-09-07 20:10:49 +0500

    devise install

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+amar = User.create(name: 'amar', email: 'amar@gmail.com', password: '123456')
